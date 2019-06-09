4aa95b365e6a1cc6553b4f8e3cfc144d998c560a
Shisei Naka <nathaniel.naka@gmail.com>
2019-06-09 03:30:43 -0400

    push our production code

---
+++ Dyna_Q.py
@@ -0,0 +1,74 @@
+import numpy as np
+import pandas as pd
+import math
+from operator import add
+
+class Dyna_Q:
+    def __init__(self, actions, gamma, alpha=0.2):
+        self.actions = actions # List
+        self.alpha = alpha # Learning rate
+        self.gamma = gamma # Discount rate (close to 0: more immediate reward, 1: more long-term reward)
+        self.q_table = pd.DataFrame(columns=self.actions) # states x actions
+        self.tc = {} # count of (state, action, state_), used to calculate probability below
+        self.t = {} # transition function: probability of seeing state_ given state and action
+        self.r = {} # reward function: expected reward of a certain action at a given state
+
+    # If the state is new, add it
+    def add_state(self, state):
+        if state not in self.q_table.index:
+            # Append the new state to q table
+            self.q_table = self.q_table.append(
+                pd.Series(
+                    np.random.rand(len(self.actions)) * 0.01,
+                    index=self.q_table.columns,
+                    name=state,
+                )
+            )
+
+            # Add the new state to T and R
+            self.tc[state] = {act : {state_ : 0.00001 for state_ in self.q_table.index} for act in self.actions}
+            self.t[state] = {act : {state_ : 1 / len(self.states) for state_ in self.q_table.index} for act in self.actions}
+            self.r[state] = {act : 0 for act in self.actions}
+
+    # Do exploitation and exploration using UCB1 algorithm
+    def choose_action(self, observation, c=1):
+        # Precondition: observation (state) is a dicrete value
+        self.add_state(observation)
+
+        # UCB1
+        max_value = 0
+        max_action = None
+        num_state = sum(sum(self.tc[observation][act].values()) for act in self.actions)
+        if num_state < 1:
+            num_state = 1
+        for act in self.actions:
+            if self.q_table.ix[observation, act] + c * math.sqrt(2 * math.log(num_state) / self.tc[observation][act]) > max_value:
+                max_action = act
+                max_value = self.q_table.ix[observation, act]
+
+        return max_action
+
+    # Happens after choosing action and observing a new state
+    def learn(self, s, a, r, s_, num_iter=100):
+        # Update model
+        self.add_state(s_)
+        self.update_Q(s, a, r, s_)
+        self.tc[s][a][s_] += 1
+        self.t[s][a][s_] = self.tc[s][a][s_] / np.sum(self.tc[s][a])
+        self.r[s][a] = (1 - self.alpha) * self.r[s][a] + self.alpha * r
+
+        # Simulation
+        for i in range(num_iter):
+            # Choose states and actions randomly
+            state = np.random.choice(self.q_table.index)
+            action = np.random.choice(self.actions)
+
+            # Get a new state and reward according to current functions and update Q
+            state_ = np.random.choice(self.q_table.index, p=list(self.t[state][action].key))
+            reward = self.r[state][action]
+            self.update_Q(state, action, reward, state_)
+        
+
+    def update_Q(self, s, a, r, s_):
+        q_target = r + self.gamma * self.q_table.ix[s_, :].max()
+        self.q_table.ix[s, a] += self.alpha * (q_target - self.q_table.ix[s, a])
---
+++ trading-bot.py
@@ -0,0 +1,178 @@
+#!/usr/bin/python
+
+# ~~~~==============   HOW TO RUN   ==============~~~~
+# 1) Configure things in CONFIGURATION section
+# 2) Change permissions: chmod +x bot.py
+# 3) Run in loop: while true; do ./bot.py; sleep 1; done
+
+from __future__ import print_function
+
+import sys
+import socket
+import json
+import time
+#import numpy as np
+
+from Dyna_Q import *
+
+# ~~~~============== CONFIGURATION  ==============~~~~
+# replace REPLACEME with your team name!
+team_name="TOURISTS"
+# This variable dictates whether or not the bot is connecting to the prod
+# or test exchange. Be careful with this switch!
+test_mode = True
+
+# This setting changes which test exchange is connected to.
+# 0 is prod-like
+# 1 is slower
+# 2 is empty
+test_exchange_index=2
+prod_exchange_hostname="production"
+
+port=25000 + (test_exchange_index if test_mode else 0)
+exchange_hostname = "test-exch-" + team_name if test_mode else prod_exchange_hostname
+
+symbols = ["BOND", "GS", "MS", "USD", "VALBZ", "VALE", "WFC", "XLF"]
+
+# ~~~~============== NETWORKING CODE ==============~~~~
+def connect():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((exchange_hostname, port))
+    return s.makefile('rw', 1)
+
+def write_to_exchange(exchange, obj):
+    json.dump(obj, exchange)
+    exchange.write("\n")
+
+def read_from_exchange(exchange):
+    return json.loads(exchange.readline())
+
+
+# ~~~~============== MAIN LOOP ==============~~~~
+def main():
+    actions = ['buy', 'sell', 'nothing']
+    gamma = 0.8
+
+    dynaQ = Dyna_Q(actions, gamma)
+
+    exchange = connect()
+    write_to_exchange(exchange, {"type": "hello", "team": team_name.upper()})
+    hello_from_exchange = read_from_exchange(exchange)
+    # A common mistake people make is to call write_to_exchange() > 1
+    # time for every read_from_exchange() response.
+    # Since many write messages generate marketdata, this will cause an
+    # exponential explosion in pending messages. Please, don't do that!
+    print("The exchange replied:", hello_from_exchange, file=sys.stderr)
+
+    symbols = ["BOND", "GS", "MS", "USD", "VALBZ", "VALE", "WFC", "XLF"]
+
+
+    trainding_history = {"BOND": [],
+                         "GS": [],
+                         "MS": [],
+                         "USD": [],
+                         "VALBZ": [],
+                         "VALE": [],
+                         "WFC": [],
+                         "XLF": [],
+                         }
+    while True:
+        from_exchange = read_from_exchange(exchange)
+        symbol = "BOND"
+        id = 0
+
+        if from_exchange['type'] != 'trade' and from_exchange['symbol'] != symbol:
+            continue
+
+        price = from_exchange['price']
+        action = dynaQ.choose_action(price)
+        id += 1
+        size = 10
+        limit = 100
+
+
+        write_to_exchange(exchange, {"type": "hello", "team": team_name.upper()})
+        hello_from_exchange = read_from_exchange(exchange)
+        position = hello_from_exchange['symbols'][symbols.index(symbol)]['position']
+
+
+        if action == 'buy' and position + size <= limit:
+            buy(symbol, price, id, size, exchange)
+
+        elif action == 'sell' and position - size >= -limit:
+            sell(symbol, price, id, size, exchange)
+
+        wait_trade_complete(exchange, symbol, size, id)
+
+        time.sleep(5)
+        #TODO nothingの時の計算がだるい
+        reward_dict = reward_calculator(price, symbol, action, exchange)
+
+        dynaQ.learn(price, action, reward_dict['reward'], reward_dict['current_trading_price'])
+
+
+def wait_trade_complete(exchange, symbol: str, size: int, id: int):
+    unfilled_size = size
+
+    while True:
+        from_exchange = read_from_exchange(exchange)
+        if from_exchange['type'] != 'fill' and from_exchange['order_id'] != id:
+            continue
+
+        unfilled_size -= from_exchange['size']
+
+        if unfilled_size == 0:
+            break
+
+
+def buy(symbol: str, price: int, id: int, size: int, exchange):
+    write_to_exchange(exchange, {"type": "add", "order_id": id, "symbol": symbol, "dir": "BUY", "price": price, "size": size})
+
+
+def sell(symbol: str, price: int, id: int, size: int, exchange):
+    write_to_exchange(exchange, {"type": "add", "order_id": id, "symbol": symbol, "dir": "SELL", "price": price, "size": size})
+
+
+def reward_calculator(ordered_price: int, symbol: str, action: str, exchange):
+    from_exchange = read_from_exchange(exchange)
+    reward_dict = {
+        'reward': 0,
+        'current_trading_price': 0
+    }
+
+    while True:
+        if from_exchange['type'] != 'trade' and from_exchange['symbol'] != symbol:
+            continue
+
+        current_trading_price = from_exchange['price']
+
+        if action == 'buy':
+            reward = current_trading_price - ordered_price
+            reward_dict['reward'] = reward
+
+        elif action == 'sell':
+            reward = ordered_price - current_trading_price
+            reward_dict['reward'] = reward
+
+        else:
+            write_to_exchange(exchange, {"type": "hello", "team": team_name.upper()})
+
+            hello_from_exchange = read_from_exchange(exchange)
+            position = hello_from_exchange['symbols'][symbols.index(symbol)]['position']
+
+            if position > 0:
+                # reward小さくするかも
+                reward = current_trading_price - ordered_price
+                reward_dict['reward'] = reward
+            else:
+                reward = ordered_price - current_trading_price
+                reward_dict['reward'] = reward
+
+        reward_dict['current_trading_price'] = current_trading_price
+        break
+
+    return reward_dict
+
+
+if __name__ == "_main_":
+    main()
